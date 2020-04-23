ab2190e9951fb725269ff6ef085acb3309be058d
cgrountree <cgrountree@gmail.com>
2020-04-23 18:22:40 -0400

    adjust home page grammar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,28 @@
   <div>
     <h1>ReadItEasy</h1>
     <p>
-      Welcome on ReadItEasy, an app that purpuse is to help you reading in one
-      of your target languages. We currently just have few books and only for
-      chinese, but more content will soon arrive !
+      Welcome to ReadItEasy, an app with the purpose of helping you read in a
+      target language. Currently, we have just a few books in only chinese, but
+      more content will arrive soon!
     </p>
     <p>
-      Start reading right now with one of my favorite chinese book :
+      Start reading right now with one of my favorite chinese books:
       <router-link :to="{ path: '/mandarin/book/活着/1' }">To Live</router-link>
-      from Hua Yu
+      by Hua Yu.
     </p>
     <br />
     <p>
       If you create an account, you can save your known words. Unknown words
-      will then lighten in red so you can see in a glimpse words you don't know
-      yet in a book.
+      will then appear in red, so you can see at a glance the words you don't
+      yet know in a book.
     </p>
     <p>
-      With this information, you can then evaluate if a word is too complex to
-      read, or if it suits your level. This level is up to you. Some people like
-      to have a smooth reading so they target books which they already know at
-      least 98% of the words. Some others are more obstinate and barely know
-      half of the words from a book. For both cases, ReadItEasy can help you
-      quickly look up definitions and statistics about a given word
+      With this information, you can then evaluate if a particular book suits
+      your level. This level is up to you. Some people like to have a smooth
+      reading experience, so they target books where they know at least 98% of
+      the words. Others are more obstinate and choose books where they barely
+      know half of the words. For both cases, ReadItEasy can help you quickly
+      look up definitions and statistics for a given word.
     </p>
   </div>
 </template>
