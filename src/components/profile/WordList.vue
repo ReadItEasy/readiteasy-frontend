828d370143ab4c60b19bfbf39d5da0e614b044ca
thebigcig <calignostic@gmail.com>
2020-12-11 22:08:14 -0500

    add new component

---
+++ src/components/profile/WordList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+      <li v-for="word in knownWords()" :key="word">
+        {{ word }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    targetLanguage: String,
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+    knownWords () {
+      return this.$store.getters.knownDict;
+  }
+},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
